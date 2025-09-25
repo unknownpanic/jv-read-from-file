c5320ae26ab9c78a95ad0bd595d2b3884c9295cc
maxima <maxima.at.work@gmail.com>
2025-09-25 21:34:23 +0300

    created filter-reading system

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,32 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        List<String> list = new ArrayList<>();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String fileLine = bufferedReader.readLine();
+
+            while (fileLine != null) {
+                String[] fileWords = fileLine.toLowerCase().split("\\W+");
+                for (String word : fileWords) {
+                    if (word.startsWith("w")) {
+                        list.add(word);
+                    }
+                }
+                fileLine = bufferedReader.readLine();
+            }
+            Collections.sort(list);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read from from file: " + fileName, e);
+        }
+        return list.toArray(new String[0]);
     }
 }
